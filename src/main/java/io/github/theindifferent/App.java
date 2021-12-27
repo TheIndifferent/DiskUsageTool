e6fdc34b8b99b87fc77d4f1a1a36600b4254895d
Stanislav Baiduzhyi <stas.baiduzhyi.dev@gmail.com>
2021-12-27 09:57:46 +0100

    Better folder choosing code.

--- src/main/java/io/github/theindifferent/App.java
+++ src/main/java/io/github/theindifferent/App.java
@@ -2,24 +2,33 @@ package io.github.theindifferent;
 
 import javax.swing.*;
 import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+import java.nio.file.Path;
+import java.util.function.Consumer;
 
-public class App {
+public class App implements Runnable {
     public static void main(String[] args) {
         System.setProperty("swing.boldMetal", "false");
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(new NimbusLookAndFeel());
-            } catch (UnsupportedLookAndFeelException e) {
-                e.printStackTrace();
-            }
-            var folderChooser = new JFileChooser(System.getProperty("user.home"));
-            folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            folderChooser.setMultiSelectionEnabled(false);
-            folderChooser.setDialogType(JFileChooser.OPEN_DIALOG);
-            folderChooser.setDialogTitle("Choose folder to scan disk usage");
-            if (folderChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                new MainWindow(folderChooser.getSelectedFile().toPath()).setVisible(true);
-            }
-        });
+        SwingUtilities.invokeLater(new App());
+    }
+
+    @Override
+    public void run() {
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        chooseDirectoryToScan(path -> new MainWindow(path).setVisible(true));
+    }
+
+    private void chooseDirectoryToScan(Consumer<Path> dirConsumer) {
+        var folderChooser = new JFileChooser(System.getProperty("user.home"));
+        folderChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        folderChooser.setMultiSelectionEnabled(false);
+        folderChooser.setDialogType(JFileChooser.OPEN_DIALOG);
+        folderChooser.setDialogTitle("Choose folder to scan disk usage");
+        if (folderChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            dirConsumer.accept(folderChooser.getSelectedFile().toPath());
+        }
     }
 }
